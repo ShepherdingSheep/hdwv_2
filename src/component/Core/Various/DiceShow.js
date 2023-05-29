ee9e440b46f4a52ea3e444d416613908219765eb
ShepherdingSheep <tg97@paran.com>
2023-05-29 19:14:49 +0900

    5.29 디자인 리뉴얼

--- src/component/Core/Various/DiceShow.js
+++ src/component/Core/Various/DiceShow.js
@@ -12,7 +12,7 @@ const DiceShow = () => {
                 <img src = {dicedata.dice[list.state.id] ? dicedata.dice[list.state.id].preview : './the_dice/multi_dice/dice_image/none_dice.png'} alt={dicedata.dice[list.state.id] ? dicedata.dice[list.state.id].kr_name : '?'}></img>
             </div>
             <div className='diceshow_desc'>
-                <div className='diceshow_name'>{dicedata.dice[list.state.id] ? dicedata.dice[list.state.id].kr_name : '주사위를 선택하세요'}</div>
+                <div className='diceshow_name'>{dicedata.dice[list.state.id] ? '행성: '+dicedata.dice[list.state.id].kr_name.substring(0,dicedata.dice[list.state.id].kr_name.length-3) : '행성: 선택하세요...'}</div>
                 <div className='diceshow_number'>{dicedata.dice[list.state.id] ? 
                 dicedata.dice[list.state.id].number_files.map((number, index) => (
                     <img key={'numberimg_'+index} className = 'dicenumber_number' src={number} alt='Number_here'></img>
